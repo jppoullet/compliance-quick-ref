25ce758169d11a855913b0979019ef95f8d51eea
jppoullet <jppoullet@yahoo.com>
2023-02-12 14:59:37 -0500

    Created scriptHowTo, rearranged css/html

--- script.js
+++ script.js
@@ -49,16 +49,3 @@ function jiraSearch() {
   window.open("https://jira.charter.com/browse/" + userInput);
   document.getElementById("jira").value = "";
 }
-
-// Triiggering dropdown menu when clicked, Closes menu when clicking anywhere else on page
-document.addEventListener("click", dropDown);
-
-function dropDown(event) {
-  const dropDown = document.querySelector(".tools-dropdown");
-
-  if (event.target.classList.contains("tools")) {
-    dropDown.classList.toggle("show");
-  } else {
-    dropDown.classList.remove("show");
-  }
-}
